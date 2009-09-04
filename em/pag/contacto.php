4d689176c90ab09625263fa40c954a2f3f2300e7
job3.14.2005 <job3.14.2005@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2009-09-04 12:10:08 +0000

    Primera version

---
+++ em/pag/contacto.php
@@ -0,0 +1,65 @@
+<?php
+		include "includes/funciones.php";
+		include "class/classes.php";
+					
+?>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Web Especialidades Metalicas S.A. 2008</title>
+
+<!-- This template was created by Mantis-a [http://www.mantisa.cz/]. For more templates visit Free website templates [http://www.mantisatemplates.com/]. -->
+
+<meta name="Description" content="..." />
+<meta name="Keywords" content="..." />
+<meta name="robots" content="all,follow" />
+<meta name="author" content="..." />
+
+<!-- Javascript -->
+<meta http-equiv="Content-Script-Type" content="text/javascript" />
+
+<!-- CSS -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen, projection, tv" />
+<link rel="stylesheet" href="css/style-print.css" type="text/css" media="print" />
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
+</head>
+<body>
+<br><br><br>
+			<div class="content-box-right2" align="center">
+				<h4>Contacte con nosotros.</h4>
+				<div class="content-box-right-in" align="center">
+					<dl>
+					<dd class="heading"><a href="#">Formulario</a></dd>
+					<dd>
+					<dd >Nombre</dd>
+					<dd ><input type="text" name="nombre"></dd>
+					<dd >Empresa</dd>
+					<dd ><input type="text" name="nombre"></dd>
+					<dd >E-mail</dd>
+					<dd ><input type="text" name="nombre"></dd>
+				</tr>
+				<tr VALIGN=middle>
+					<dd >Asunto</dd>
+					<dd ><input type="text" name="nombre"></dd>
+
+					<dd >Mensaje</dd>
+					<dd><input type="text" name="nombre"></d>
+			
+					<dd><input type="submit" ></dd>
+
+		</dd>
+					</dl>
+				</div>
+			<!-- Content right end -->
+						<div id="column-right-bottom">&nbsp;</div>
+			</div>
+		
+			<!-- Content left end -->
+
+
+
+
+</body>
+</html>
---
+++ em/pag/main.php
@@ -0,0 +1,24 @@
+
+	<div class='titulo'>Bienvenidos a la web de Especialidades Metalicas </div>
+    <div class='contenido' style='width:500px;'> CD-Text es una extensión de las especificaciones del Disco Compacto de Audio recogidas en 
+    el Libro Rojo. Permite el almacenamiento de información adicional (por ejemplo: nombre del álbum, título de la 
+    canción y artista) sobre un CD de audio estándar. La información se almacena en la pista de entrada del CD, donde
+    hay aproximadamente cinco kilobytes de espacio disponible, o en los subcanales R a W en el disco, que pueden almacenar
+    alrededor de 31 megabytes. Estas áreas no se usan por los CDs acordes al Libro Rojo. El texto se almacena en un
+    formato utilizable por el Sistema Interactivo de Transmisión de Texto (ITTS en sus siglas en inglés). El ITTS
+    también se utiliza en la difusión de audio digital (DAB) o el MiniDisc. Las especificaciones se publicaron en
+    septiembre de 1996, bajo el amparo de Sony. 
+    El soporte de CD-Text es común, pero no universal. Existen utilidades para extraer la información contenida e 
+	</div>
+	<div class='img' style='position: absolute; right: 20px; top: 150px;'><img src='img/emsa/puertaprin.png' /></div>
+	<div class='titulo'>Noticias</div>
+    <div class='contenido' style='width:500px;'>
+    <b>Nueva Web Emsa 2008<b><br>					
+		Nos complace presentarles la nueva web de Emsadonde podran consultar los productos que tenemos en
+		 stock sin moverse de casa.
+	<br><br>
+	<b>Lista de Productos en Oferta<b><br>					
+		Conjunto de productos en oferta , lista completa de la BBDD con marca en oferta.
+	</div>
+			
+
