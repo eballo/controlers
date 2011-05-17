f52b0a9ca4dd8be6cb488572c1be761b7ba36054
job3.14.2005 <job3.14.2005@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-05-17 10:13:20 +0000

    SACSEX-V.0.1

--- sacsex/bajas.php
+++ sacsex/bajas.php
@@ -5,17 +5,18 @@
    $id=$_GET['id']; 
    $query="delete from user where ID = $id";
    $x=mysql_query($query,$link);
+   
    if ($x==1){
    		$miss="Usuari eliminat";
    		echo "<script type='text/javascript'>
 			alert('$miss');
-			document.location = 'administrador.php';
+			document.location = 'admin.php';
 		</script>";
    }else{
    		$miss="Error al intentar Eliminar al usuari";
 	   	echo "<script type='text/javascript'>
 			alert('$miss');
-			document.location = 'administrador.php';
+			document.location = 'admin.php';
 		</script>";
    }
 ?>
