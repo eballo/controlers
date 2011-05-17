8a5c22f77923f51597acb6f805a9d271756db0ad
giodelabarrera <giodelabarrera@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-05-17 23:09:43 +0000

    modificacion de altas y eliminacion de filepaths basico del usuario

--- trunk/sacsex/altafile.php
+++ trunk/sacsex/altafile.php
@@ -1,22 +1,22 @@
 <?php 
-include_once './includes/headers.php';
+	include_once './includes/headers.php';
+	
+	$id=$_SESSION['id'];
+	$filepath=$_POST['filepath'];
+	$alta=$_POST['alta'];
+	echo $id;
+	echo $filepath;
+	echo $alta;
 
-$id=$_SESSION['id'];
-$filepath=$_POST['filepath'];
-$alta=$_POST['alta'];
-echo $id;
-echo $filepath;
-echo $alta;
-
-if ($alta == true){
-	$link=conectar('bdsintesi');
-	$query="INSERT INTO filepath (FILEPATH,USER_ID) VALUES ('$filepath',$id)";
-	$result=mysql_query($query,$link);
-	desconectar($link);
-
-	echo"<script type='text/javascript'>
-		document.location = 'search.php';
-	</script>";
-}
+	if ($alta == true){
+		$link=conectar('bdsintesi');
+		$query="INSERT INTO filepath (FILEPATH,USER_ID) VALUES ('$filepath',$id)";
+		$result=mysql_query($query,$link);
+		desconectar($link);
+	
+		echo"<script type='text/javascript'>
+			document.location = 'search.php';
+		</script>";
+	}
 
 ?>
