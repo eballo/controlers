33e78e960af192872429f22cb26beee145408e2a
kirsley <kirsley@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-05-23 20:14:37 +0000

    Fusionado el codigo de introducir y eliminar directorios de la bd en el search

--- sacsex/search.php
+++ sacsex/search.php
@@ -1,18 +1,38 @@
 <?php
 	include_once 'includes/headers.php';
 	include_once 'includes/userAuthValidation.php';
-
-	$id_user=$_SESSION['id'];
+	include_once 'includes/libsheader.php';
 	$link=conectar('bdsintesi');
-	$query="SELECT USER_ID,FILEPATH,IDF FROM filepath WHERE USER_ID=$id_user";
+	
+	$id=$_SESSION['id'];
+	if (isset($_GET['accion'])){
+		$accion=$_GET['accion'];
+		echo $accion;
+		if ($accion=="subir"){
+			$filepath=$_POST['filepath'];
+			$insQuery="INSERT INTO filepath (FILEPATH,USER_ID) VALUES ('$filepath',$id)";
+			$res=mysql_query($insQuery,$link);
+			if (!$res){
+				$error="Error: No se ha podido realizar la insercion";
+			}
+		}elseif($accion=="borrar"){
+			$idFile=$_GET['idFile'];	
+			$delQuery="DELETE FROM filepath WHERE IDF=$idFile";
+			$rest=mysql_query($delQuery,$link);
+			if (!$rest){
+				$error="No se pudo eliminar el directorio indicado";
+			}
+		}
+	}	
+	$query="SELECT USER_ID,FILEPATH,IDF FROM filepath WHERE USER_ID=$id";
 	$result=mysql_query($query,$link);
 	$rows=mysql_num_rows($result);
-	echo "<h2> Modificacion de FilesPath </h2>";
+	echo "<h2> Modificacion de rutas de Directorio </h2>";
 	if ($rows >0){
-		echo "<h3> Files: </h3>";
-		echo "<table><tr>PATH FILE de Usuario:</tr>";
+		echo "<h3> Directorios: </h3>";
+		echo "<table><tr><th colspan='2'>Rutas Del Usuario:</th></tr>";
 		while($row=mysql_fetch_array($result)){
-			printf("<tr><td>%s</td><td><a href=\"bajafile.php?idFile=%d\">Borrar</a></td></tr>",$row['FILEPATH'],$row['IDF']);
+			printf("<tr><td>%s</td><td><a href=\"search.php?accion=borrar&idFile=%d\">Eliminar</a></td></tr>",$row['FILEPATH'],$row['IDF']);
 		}
 		echo "</table><br>";
 	}
@@ -25,12 +45,20 @@
 	<title>Alta de Files de Usuario</title>
 </head>
 <body>
+<?php include_once 'includes/cabecera.php';?>
+<table><tr><td rowspan='2'></table>
 	<h3> Alta de File: </h3>
-	<form action='altafile.php' method='post'>
+	<form action='search.php?accion=subir' method='post'>
 		File: <input type="text" name='filepath' />
 		<input type="submit" value='valida' /><br />
-		<input type='hidden' name='alta' value='true' />
 	</form>
 	<a href='asistenteBusqueda.php'>Buscar</a>
+	<?php 
+	if ($error!=''){
+			echo "<script type='text/javascript'>
+				alert('$error');
+			</script>";
+		}
+	?>
 </body>
 </html>
