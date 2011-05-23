7e89e99e1fac82729368461915ceee73ba096b16
job3.14.2005 <job3.14.2005@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-05-23 11:46:21 +0000

    SACSEX

---
+++ trunk/sacsex/includes/cabecera.php
@@ -0,0 +1,8 @@
+<div class="headerMain">
+	<div class="tituloHeader">
+		Sacsex V.1.0
+	</div>
+	<div class="cierreSesion">
+		<input type="button" value="Cerrar Sesion" onclick="javascript: document.location='logout.php'"/>
+	</div>
+</div>
