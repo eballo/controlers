d09aa1d9a9ae477492f26832c80eaf9e7c7621e6
giodelabarrera <giodelabarrera@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-05-25 09:58:20 +0000

    search retocarlo

--- trunk/sacsex/search.php
+++ trunk/sacsex/search.php
@@ -2,33 +2,71 @@
 	include_once 'includes/headers.php';
 	include_once 'includes/userAuthValidation.php';
 	include_once 'includes/libsheader.php';
-	include_once 'includes/cabecera.php';
+	//include_once 'includes/cabecera.php';
 	
 	$link=conectar('bdsintesi');
 	
+	/*$postsubir=$_POST['accion'];
+	echo "Si weeey",$postsubir,"<br>";*/
+	
+	if (isset($_GET['error'])){
+		$error = $_GET['error'];
+	} else{
+		$error = "";
+	}
+	
 	$id=$_SESSION['id'];
 	if (isset($_GET['accion'])){
-		$accion=$_GET['accion'];
-		if ($accion=="subir"){
-			$filepath=$_POST['filepath'];
-			$insQuery="INSERT INTO filepath (FILEPATH,USER_ID) VALUES ('$filepath',$id)";
-			$res=mysql_query($insQuery,$link);
-			if (!$res){
-				$error="Error: No se ha podido realizar la insercion";
-			}
-		}elseif($accion=="borrar"){
-			$idFile=$_GET['idFile'];	
-			$delQuery="DELETE FROM filepath WHERE IDF=$idFile";
-			$rest=mysql_query($delQuery,$link);
-			if (!$rest){
-				$error="No se pudo eliminar el directorio indicado";
+		$accion = $_GET['accion'];
+		if ($accion == "subir"){
+			$filepath = $_POST['filepath'];
+			if ($filepath != ""){
+				$insQuery="INSERT INTO filepath (FILEPATH,USER_ID) VALUES ('$filepath',$id)";
+				$res=mysql_query($insQuery,$link);
+				if (!$res){
+					echo "<script type='text/javascript'>
+						alert('Error No se ha podido realizar la insercion');
+					</script>";
+				}
+			} else {
+				echo "<script type='text/javascript'>
+					document.location = 'search.php?error=1';
+				</script>";
 			}
 		}
-	}	
+		elseif($accion == "borrar"){
+			$idFile = $_GET['idFile'];	
+			$delQuery = "DELETE FROM filepath WHERE IDF=$idFile";
+			$res = mysql_query($delQuery,$link);
+			if (!$res){
+				echo "<script type='text/javascript'>
+					alert('No se pudo eliminar el directorio indicado');
+				</script>";
+				/*$error="No se pudo eliminar el directorio indicado";*/
+			}
+		}
+	}
+?>
+<!--
+<HTML>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Opciones del Usuario</title>
+</head>
+<body>
+	<h3> OPCIONES </h3>
+				<script type='text/javascript'>
+					document.location = 'search.php?accion=subir';
+				</script>";
+
+</body>
+-->
+<?php
+/* Borrar */
 	$query="SELECT USER_ID,FILEPATH,IDF FROM filepath WHERE USER_ID=$id";
 	$result=mysql_query($query,$link);
 	$rows=mysql_num_rows($result);
-	echo "<h2> Modificacion de rutas de Directorio </h2>";
+	echo "<h2> Modificacion de rutas de Directorio (En construccion)</h2>";
 	if ($rows >0){
 		echo "<h3> Directorios: </h3>";
 		echo "<table><tr><th colspan='2'>Rutas Del Usuario:</th></tr>";
@@ -37,7 +75,6 @@
 		}
 		echo "</table><br>";
 	}
-	desconectar($link);
 
 ?>
 <html>
@@ -46,20 +83,30 @@
 	<title>Alta de Files de Usuario</title>
 </head>
 <body>
-
-<table><tr><td rowspan='2'></table>
+	
 	<h3> Alta de File: </h3>
-	<form action='search.php?accion=subir' method='post'>
+	<form action='search.php?accion=subir' method='POST'>
+		<!-- <input type='hidden' name='accion' value='subir'/> -->
+		<div id='divErrors' style='color: red'>		
+			<?php
+			if ($error){
+				switch($error){
+					case 1:
+						/* Error al validar un archivo nulo */
+						echo "Directorio nulo no valido.";break;
+						
+					/*echo "<script type='text/javascript'>
+						alert('$error');
+					</script>";*/
+				}
+			}
+			?>
+		</div>
 		File: <input type="text" name='filepath' />
 		<input type="submit" value='valida' /><br />
 	</form>
 	<a href='asistenteBusqueda.php'>Buscar</a>
-	<?php 
-	if ($error!=''){
-			echo "<script type='text/javascript'>
-				alert('$error');
-			</script>";
-		}
-	?>
+
 </body>
 </html>
+<?php desconectar($link); ?>
