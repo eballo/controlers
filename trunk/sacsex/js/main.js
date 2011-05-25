31e8d6f12355cc9e1c86f635843f1fb6d3ad546d
kirsley <kirsley@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-05-25 16:29:42 +0000

    modificacion del apartado de busqueda

--- trunk/sacsex/js/main.js
+++ trunk/sacsex/js/main.js
@@ -9,3 +9,11 @@
 	function ocultaForm(){
 		$("#buscaForm").fadeOut("slow");
 	}
+	function configuracion(){
+		$("#searchFiles").fadeIn("slow");
+		//$("#buscaForm").fadeOut("slow");
+	}
+	function busqueda(){
+		$("#searchFiles").fadeOut("slow");
+		//$("#buscaForm").fadeIn("slow");		
+	}
