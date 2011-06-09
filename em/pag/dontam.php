f74b7d1b5af9f446bc362dcef4d8a9ae044de4f8
job3.14.2005 <job3.14.2005@ca4e47cc-7186-11de-a0a7-fb2681086dbc>
2011-06-09 11:18:36 +0000

    SACSEX.V.1.0.1

--- em/pag/dontam.php
+++
@@ -1,8 +0,0 @@
-<div class='logoemsa'></div>
-<div style='text-align:center; margin-top: 20px;'>
-<a style='border:0px;' href='http://maps.google.es/maps?hl=es&ie=UTF-8&q=especialidades+metalicas&near=Barcelona&fb=1&cid=18194279175792615900&li=lmd&z=14&t=m' target='blank'>
-<img border=0 src="img/mapa.png"/>
-</a>
-</div>
-<div class='piepa'>Estamos en la calle C/ Mineria 54-56, 08038 Barcelona <br>
- Para mas información <b>Telf:</b> 933323716 <b>Fax:</b> 933310193 <b>Mail:</b> ventas@emsa-es.com</div>
--- emadmin/seguridad/seguridad.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (! $_SESSION['login']){
-	include "login.php";
-	die;
-}
-
-
-?>
